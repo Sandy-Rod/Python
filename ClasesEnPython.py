e77d79a7c4434f82a92c426a6365349c16ed67e6
Sandy Rodriguez <sandy.rodriguezaponte@hotmail.com>
2024-05-29 13:41:55 +0200

    getters and setters

--- ClasesEnPython.py
+++ ClasesEnPython.py
@@ -66,7 +66,35 @@ class MyCar:
 
 
 myCar1 = MyCar()
-print(myCar1._name_protected)
+print("\n \n imprimir un método protegido \t myCar1._name_protected:  \t ", myCar1._name_protected)
+# al ser un método privado no se puede acceder, nos da un error
 #print(myCar1.__name_private)
+#pero hay un "truco" y es poniendo objeto, punto, guión bajo, nombre de la clase, guión bajo, guión bajo y nombre del método
+print("\n \n imprimir un método privado \t myCar1._MyCar__name_private:  \t ", myCar1._MyCar__name_private)
 
 
+
+# =============================================================================
+#      Getters and Setters
+# =============================================================================
+
+
+class GetterSetterClass:
+    _mi_atributo_protegido = "This is a static variable"
+    __mi_atributo_privado = "This is a static variable"
+
+    def __init__(self, mi_atributo_protegido, mi_atributo_privado):
+        self._mi_atributo_protegido = mi_atributo_protegido
+        self.__mi_atributo_privado = mi_atributo_privado
+
+    @property #get
+    def mi_atributo_privado(self):
+        return self.__mi_atributo_privado + " 2"
+    
+    @mi_atributo_privado.setter
+    def mi_atributo_privado(self, value):
+        self.__mi_atributo_privado = value + " 1"
+
+claseGetterSetters = GetterSetterClass(mi_atributo_protegido="estoy protegido", mi_atributo_privado="soy privado")
+claseGetterSetters.mi_atributo_privado = "soy una variable privada"
+print("\n \n primero entra en el método 'setter' que tiene la anotación @mi_atributo_privado.setter  y después entra en el método 'get' que tiene la anotación @property \t claseGetterSetters.mi_atributo_privado:  \t ", claseGetterSetters.mi_atributo_privado)
